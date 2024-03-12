af08b2d891a9772685b8c5c8789b257801d02b94
miberezkin <vnmslf@gmail.com>
2024-03-13 00:07:25 +0300

    MAR-12: catalog for index page, catalog__listing tmplt for index + catalog

--- local/templates/vector/components/bitrix/news/global__catalog/news.php
+++ local/templates/vector/components/bitrix/news/global__catalog/news.php
@@ -21,7 +21,7 @@ $this->setFrameMode(true);?>
 );?>
 <?$APPLICATION->IncludeComponent(
 	'bitrix:news.list',
-	'',
+	'catalog__listing',
 	[
 		'IBLOCK_TYPE' => $arParams['IBLOCK_TYPE'],
 		'IBLOCK_ID' => $arParams['IBLOCK_ID'],
