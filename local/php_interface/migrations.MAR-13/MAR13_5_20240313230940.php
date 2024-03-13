f06442c5e04d5d61819aafe3075cc0b1f6f7030d
miberezkin <vnmslf@gmail.com>
2024-03-13 23:10:30 +0300

    MAR-13: static + hero section for index page

---
+++ local/php_interface/migrations.MAR-13/MAR13_5_20240313230940.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class MAR13_5_20240313230940 extends Version
+{
+    protected $description = "";
+
+    protected $moduleVersion = "4.6.2";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+
+        $iblockId = $helper->Iblock()->getIblockIdIfExists(
+            'static',
+            'Static'
+        );
+
+        $helper->Iblock()->addSectionsFromTree(
+            $iblockId,
+            array (
+)        );
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
---
+++ local/php_interface/migrations.MAR-13/MAR13_6_20240313231004.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Sprint\Migration;
+
+class MAR13_6_20240313231004 extends Version
+{
+    protected $description   = "";
+    protected $moduleVersion = "4.6.2";
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+             ->IblockElementsImport()
+             ->setExchangeResource('iblock_elements.xml')
+             ->setLimit(20)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                      ->Iblock()
+                      ->addElement(
+                          $item['iblock_id'],
+                          $item['fields'],
+                          $item['properties']
+                      );
+             });
+    }
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function down()
+    {
+    }
+}
